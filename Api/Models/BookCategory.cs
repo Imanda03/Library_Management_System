c68a875b966f33a65dca03c69c5d7ba19a35d542
Your Name <asis03ktm@gmail.com>
2023-11-01 17:16:53 +0545

    backend work finished

---
+++ Api/Models/BookCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models
+{
+    public class BookCategory : ModelBase
+    {
+        public string Category { get; set; } = string.Empty;
+        public string SubCategory { get; set; } = string.Empty;
+
+    }
+}
